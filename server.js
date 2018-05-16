a19bdb6e9e7eb02cc50833737876499f83a791a1
Lakshay Aggarwal <lakshay.aggarwal@nagarro.com>
2018-05-16 15:28:25 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/ServerApp'));
+
+app.get('/*all', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/ServerApp/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
